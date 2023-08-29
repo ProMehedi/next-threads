c66ee3e4a1eeb4b88ce5d3950e5fee86d88b3ab1
promehedi <admin@promehedi.com>
2023-08-29 12:41:32 +0600

    Create Topbar Component

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -21,9 +21,6 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en'>
-        <head>
-          <link rel='shortcut icon' href='favicon.png' type='image/x-icon' />
-        </head>
         <body className={inter.className}>
           <Topbar />
           <main>
