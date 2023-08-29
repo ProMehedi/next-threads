4ffdb4d8294115a80d2bde7d4d1cd865c972daa6
promehedi <admin@promehedi.com>
2023-08-29 13:08:06 +0600

    Create Leftsidebar Component

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,8 +1,63 @@
+'use client'
+
+import { sidebarLinks } from '@/constants'
+import { SignOutButton, SignedIn } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
+
 const LeftSidebar = () => {
+  const router = useRouter()
+  const pathname = usePathname()
+
   return (
-    <div>
-      <h1>This is LeftSidebar</h1>
-    </div>
+    <section className='custom-scrollbar leftsidebar'>
+      <div className='flex w-full flex-1 flex-col gap-6 px-6'>
+        {sidebarLinks.map((link, id) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route
+          return (
+            <Link
+              href={link.route}
+              key={id}
+              className={
+                isActive
+                  ? 'bg-primary-500 leftsidebar_link'
+                  : 'leftsidebar_link'
+              }
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+            </Link>
+          )
+        })}
+      </div>
+      <div className='mt-10 px-6'>
+        <SignedIn>
+          <SignOutButton
+            signOutCallback={() => {
+              router.push('/')
+            }}
+          >
+            <div className='flex gap-4 p-4 curson-pointer'>
+              <Image
+                src='/icons/logout.svg'
+                alt='Sign Out'
+                width={24}
+                height={24}
+              />
+              <p className='text-light-2 max-lg:hidden'>Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
+      </div>
+    </section>
   )
 }
 
