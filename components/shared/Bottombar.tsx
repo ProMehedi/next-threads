9a0d165097e9edf619324eebd02583253c6ce34a
promehedi <admin@promehedi.com>
2023-08-29 13:14:26 +0600

    Create Bottombar Component

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,8 +1,43 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+//
+import { sidebarLinks } from '@/constants'
+
 const Bottombar = () => {
+  const pathname = usePathname()
+
   return (
-    <div>
-      <h1>This is Bottombar</h1>
-    </div>
+    <section className='bottombar'>
+      <div className='bottombar_container'>
+        {sidebarLinks.map((link, id) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route
+          return (
+            <Link
+              href={link.route}
+              key={id}
+              className={
+                isActive ? 'bg-primary-500 bottombar_link' : 'bottombar_link'
+              }
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className='text-subtle-medium text-light-1 max-sm:hidden'>
+                {link.label.split(/\s/)[0]}
+              </p>
+            </Link>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
