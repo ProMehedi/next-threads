250291e414a6403249ca6e35c81149bdcfad7e5a
promehedi <admin@promehedi.com>
2023-09-11 04:14:04 +0600

    Display Threads in Homepage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,37 @@
-import { UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs'
+// Components
+import { Thread } from '@components/cards'
+// Utils
+import { fetchPosts } from '@utils/actions/threadActions'
 
-const Home = () => {
+const Home = async () => {
+  const result = await fetchPosts(1, 30)
+
+  const user = await currentUser()
   return (
     <>
       <h1 className='head-text text-left'>Welcome to Threads!</h1>
-      <UserButton afterSignOutUrl='/' />
+
+      <div className='mt-9 flex flex-col gap-10'>
+        {result.posts.length == 0 && (
+          <p className='no-result'>No threads found!</p>
+        )}
+
+        {result.posts.length > 0 &&
+          result.posts.map((post) => (
+            <Thread
+              key={post._id}
+              id={post._id}
+              currentUserId={user?.id || ''}
+              parentid={post.parentId}
+              content={post.text}
+              author={post.author}
+              community={post.community}
+              createdAt={post.createdAt}
+              comments={post.children}
+            />
+          ))}
+      </div>
     </>
   )
 }
