c8761ef1afb8ef8eca6d2b9f287eded7abdf0a97
promehedi <admin@promehedi.com>
2023-09-11 22:34:26 +0600

    Setup Community Webhook

--- app/(home)/communities/page.tsx
+++ app/(home)/communities/page.tsx
@@ -1,7 +1,19 @@
+import { redirect } from 'next/navigation'
+import { currentUser } from '@clerk/nextjs'
+// Components
+import { Activity as ActivityCard } from '@components/cards'
+// Utils
+import { fetchUser, getActivity } from '@utils/actions/userActions'
+
 const Communities = async () => {
+  const user = await currentUser()
+  if (!user) return null
+
+  const userInfo = await fetchUser(user.id)
+  if (!userInfo?.onboarded) redirect('/onboarding')
   return (
     <section>
-      <h1 className='head-text mb-10'>This is Communities</h1>
+      <h1 className='head-text mb-10'>Communities</h1>
     </section>
   )
 }
